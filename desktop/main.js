5e4394dfa35ba7fa2895c63c1014713c21237155
DJDams1 <dami.scoot3+3@gmail.com>
2025-01-12 18:27:06 +0100

    ajout des menu (desktop)

--- desktop/main.js
+++ desktop/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu , shell} = require('electron');
 const path = require('path');
 
 let mainWindow;
@@ -12,7 +12,7 @@ app.on('ready', () => {
   // Créer la fenêtre principale
   mainWindow = new BrowserWindow({
     width: 1920,
-    height: 1200,
+    height: 1250,
     icon: iconPath, // Utilise directement l'icône placée à la racine
     webPreferences: {
       nodeIntegration: false,
@@ -24,7 +24,45 @@ app.on('ready', () => {
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
-});
+
+  const customMenu = Menu.buildFromTemplate([
+    {
+      label: 'Application',
+      submenu: [
+        {label: 'À propos',accelerator: 'CmdOrCtrl+a', click: () => {shell.openExternal('https://github.com/djdams1/ChatOFF/wiki'); }// Ouvre un lien dans le navigateur}
+        },
+        { type: 'separator' },{label: 'Quitter',accelerator: 'CmdOrCtrl+q',click: () => app.quit()},
+      ],
+    },
+    
+    {
+      label: 'Affichage',
+      submenu: [
+        { label: 'Rafraîchir', role: 'reload' },
+        { label: 'Outils de développement', role: 'toggleDevTools' },
+        { type: 'separator' },
+        { label: 'Plein écran', role: 'togglefullscreen' },
+      ],
+    },
+    // Ajouter "Support" directement sans sous-menu
+    {
+      label: 'Support',
+      accelerator: 'CmdOrCtrl+s', // Cmd+Q sur macOS, Ctrl+Q sur Windows/Linux
+      click: () => {
+        const supportWindow = new BrowserWindow({
+          icon: iconPath,
+          width: 1110,
+          height: 1000,
+          webPreferences: {
+            nodeIntegration: false,
+          },
+        });
+        supportWindow.loadURL('https://tally.so/r/mOdbKa'); // Charge le lien dans une nouvelle fenêtre
+      },
+    },
+  ]);
+  Menu.setApplicationMenu(customMenu);
+  
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -37,3 +75,4 @@ app.on('activate', () => {
     createWindow();
   }
 });
+})
